6e886fa884bfcc089e0026b19202b790b77b52a5
Carsten <cfrede17@student.aau.dk>
2020-03-10 16:31:01 +0100

    Added Main Menu
    empty buttons, simple shapes and one-takes

---
+++ ScalienAbduction/Assets/Scripts/MenuScripts/LookAtScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtScript : MonoBehaviour
+{
+
+    public GameObject target;
+    public float speedModifier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target.transform.position + new Vector3(0.0f, -4.0f, 5.0f));
+        transform.RotateAround(target.transform.position, new Vector3(0.0f,0.1f,0.0f), 20 * Time.deltaTime * speedModifier);
+        
+    }
+}
