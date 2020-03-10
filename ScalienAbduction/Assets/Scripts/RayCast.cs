58a63013dba0dba358bd3b3471213caf49b36f9e
Oenger17 <Oenger17@student.aau.dk>
2020-03-10 12:10:06 +0100

    Added RayCast
    Checks distance and tag of object before running code in if statement.

---
+++ ScalienAbduction/Assets/Scripts/RayCast.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCast : MonoBehaviour
+{
+    public RaycastHit hit; // To get information back from Raycast
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HitByRay();
+    }
+    void HitByRay()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out hit)) // If object is hit by raycast
+        {
+            if (hit.distance > 3 && hit.transform.tag == "SuitableTagName")
+            {
+                // Do something, enable to pick-up etc.
+            }
+        }
+    }
+}
