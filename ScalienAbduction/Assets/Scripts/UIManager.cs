9dc897f96d6e3f6d9d836b19fc5ca136ffcc099b
exhex <c.kritik@gmail.com>
2020-03-11 14:25:05 +0100

    Added 'functional' menu + secrets
    can you find the secret in baseScene?

---
+++ ScalienAbduction/Assets/Scripts/UIManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+
+    public Button bStart, bSettings, bExit;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bStart.onClick.AddListener(() => ChangeScene(1));
+        bSettings.onClick.AddListener(() => ChangeScene(0));
+        bExit.onClick.AddListener(QuitApplication);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void ChangeScene(int scene) 
+    {
+        switch (scene) 
+        {
+            case 0:
+                Debug.Log("opening settings..");
+                SceneManager.LoadScene("baseScene");
+                break;
+            case 1:
+                Debug.Log("scene change pending..");
+                SceneManager.LoadScene("1stLevel_Test");
+                break;
+            default:
+                break;
+        }
+    }
+    void QuitApplication() 
+    {
+        Debug.Log("quitting..");
+        Application.Quit();
+    }
+}
