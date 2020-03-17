07fa4e623094850606c467bcaa0cb6177b6160ca
Oenger17 <oenger17@student.aau.dk>
2020-03-17 18:46:24 +0100

    Collision Fix
    Fixed collisions with players and walls
    - Fixed pick-up, should be much more smoothe
    - Fixed push, you can push forward now
    - Raised boxes from ground slightly when scalin
    
    Collisions are overall much better, but there are still issues when a cube is scaled next to another cube.

--- ScalienAbduction/Assets/Scripts/RayCast.cs
+++ ScalienAbduction/Assets/Scripts/RayCast.cs
@@ -5,46 +5,40 @@ using UnityEngine;
 public class RayCast : MonoBehaviour
 {
     public RaycastHit hit; // To get information back from Raycast
-    public GameObject TheHitBox;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject TheHitBox; // To store the box that was hit
 
     // Update is called once per frame
     void Update()
     {
-        HitByRay();
+        HitByRay(); // Run HitByRay
     }
+
     void HitByRay()
     {
-        if (Physics.Raycast(transform.position, transform.forward, out hit, maxDistance: 15))
+        if (Physics.Raycast(transform.position, transform.forward, out hit, maxDistance: 15)) // If the raycast strikes a collider within 15 distance
         {
-            Debug.Log("Looking at: " + hit.transform);
-            if (hit.transform.tag == "Small Cube" || hit.transform.tag == "Medium Cube")
+            if (hit.transform.tag == "Small Cube" || hit.transform.tag == "Medium Cube") // If object has suitable tag
             {
-                if (Input.GetKeyDown("e"))
+                if (Input.GetKeyDown("e")) // If 'e' is pressed
                 {
-                    if (TheHitBox == null)
+                    if (TheHitBox == null) // and if there is no GameObject stored in TheHitBox
                     {
-                        if(hit.transform.tag == "Small Cube") { hit.transform.SendMessage("pickUp"); }
-                        if (hit.transform.tag == "Medium Cube") { hit.transform.SendMessage("push"); }
-                        TheHitBox = hit.collider.gameObject;
+                        if(hit.transform.tag == "Small Cube") { hit.transform.SendMessage("pickUp"); } // If small cube, send pickup message
+                        if (hit.transform.tag == "Medium Cube") { hit.transform.SendMessage("push"); } // If medium cube, send push message
+                        TheHitBox = hit.collider.gameObject; // Set TheHitBox gameObject to the gameObject of the hit collider
                     }
-                    else { TheHitBox = null; }
+                    else { TheHitBox = null; } // If TheHitBox gameObject was not empty, make it empty.
                 }
             }
-                if (Input.GetKeyDown("r") && (hit.transform.tag == "Small Cube" || hit.transform.tag == "Medium Cube" || hit.transform.tag == "Large Cube"))
+
+                if (Input.GetKeyDown("r") && (hit.transform.tag == "Small Cube" || hit.transform.tag == "Medium Cube" || hit.transform.tag == "Large Cube")) // If suitable tag and 'r' is pressed
                 {
-                    //Run code when pressing 'r' on cube, scale up.
-                    hit.transform.SendMessage("downScale");
+                    hit.transform.SendMessage("downScale"); // send message downScale
                 }
-                if (Input.GetKeyDown("t") && (hit.transform.tag == "Small Cube" || hit.transform.tag == "Medium Cube" || hit.transform.tag == "Large Cube"))
+                if (Input.GetKeyDown("t") && (hit.transform.tag == "Small Cube" || hit.transform.tag == "Medium Cube" || hit.transform.tag == "Large Cube")) // If suitable tag and 't' is pressed
                 {
-                    //Run code when pressing 't' on cube, scale up.
-                    hit.transform.SendMessage("upScale");
+                    hit.transform.SendMessage("upScale"); // send message upScale
                 }
-            }
         }
     }
+}
--- ScalienAbduction/Assets/Scripts/Scaling.cs
+++ ScalienAbduction/Assets/Scripts/Scaling.cs
@@ -5,40 +5,32 @@ using UnityEngine;
 public class Scaling : MonoBehaviour
 {
 
-    Vector3 scaleChange;
-
-    bool overColliding = false;
-
-    Vector3 smallCube = new Vector3(200f, 200f, 200f);
+    Vector3 smallCube = new Vector3(200.0f, 200.0f, 200.0f);
     Vector3 mediumCube = new Vector3(500f, 500f, 500f);
     Vector3 largeCube = new Vector3(1000f, 1000f, 1000f);
 
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
     void upScale()
     {
+
         Vector3 newScale = transform.localScale; // What is the current size?
 
-        if (newScale == smallCube && !overColliding)
+        if (this.transform.tag == "Large Cube")
         {
-            transform.localScale = mediumCube;
-            this.tag = "Medium Cube";
+            //Insert something?
         }
 
-        if (newScale == mediumCube && !overColliding)
+        if (this.transform.tag == "Medium Cube")
         {
+            transform.position = transform.position + new Vector3(0, 3, 0);
             transform.localScale = largeCube;
             this.tag = "Large Cube";
         }
 
-        if (newScale == largeCube && !overColliding)
+        if (this.transform.tag == "Small Cube")
         {
-            Debug.Log("Cube is too large!");
+            transform.position = transform.position + new Vector3(0,1.5f,0);
+            transform.localScale = mediumCube;
+            this.tag = "Medium Cube";
         }
     }
 
@@ -46,34 +38,21 @@ public class Scaling : MonoBehaviour
     {
         Vector3 newScale = transform.localScale; // What is the current size?
 
-        if (newScale == smallCube && !overColliding)
+        if (this.transform.tag == "Small Cube")
         {
-            Debug.Log("Cube is too small!");
+            // Insert something?
         }
 
-        if (newScale == mediumCube && !overColliding)
+        if (this.transform.tag == "Medium Cube")
         {
             transform.localScale = smallCube;
             this.tag = "Small Cube";
         }
 
-        if (newScale == largeCube && !overColliding)
+        if (this.transform.tag == "Large Cube")
         {
             transform.localScale = mediumCube;
             this.tag = "Medium Cube";
         }
-    }
-
-    void OnCollisionStay(Collision collision)
-    {
-        if (collision.collider.tag == "Player")
-            overColliding = true;
-        Debug.Log("Colliding");
-    }
-    void OnCollisionExit(Collision collision)
-    {
-        if (collision.collider.tag == "Player")
-            overColliding = false;
-        Debug.Log("Colliding");
     }
 }
