59a7cf5259f2bcfb8ef4cc5fa5ad1b100434091a
exhex <c.kritik@gmail.com>
2020-03-21 17:39:37 +0100

    Fixed jumping close to objects + etc
    added: 
    slopeLimit hack to make jumping close to objects act as intended
    New locations for cubes based on feedback
    New jump height and movespeed based on feedback
    
    Known issues:
    pickup only works every other time (hvemerhveranden?)
    sometimes cubes lose gravity and collision, leaving them hovering in air (introduced with slopeLimit hack??)

--- ScalienAbduction/Assets/Scripts/CharacterScripts/PlayerMovement.cs
+++ ScalienAbduction/Assets/Scripts/CharacterScripts/PlayerMovement.cs
@@ -25,17 +25,20 @@ public class PlayerMovement : MonoBehaviour
         if(isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
+            if(controller.slopeLimit != 45) controller.slopeLimit = 45;
+
         }
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-
-        Vector3 move = transform.right * x + transform.forward * z;
+        
+        Vector3 move = transform.right * (x * 0.75f)  + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
+            controller.slopeLimit = 90;
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
