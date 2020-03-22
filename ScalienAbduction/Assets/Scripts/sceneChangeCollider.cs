24432ecb92eba497278ebc38f0f2c5509377c6fb
Oenger17 <oenger17@student.aau.dk>
2020-03-22 04:14:45 +0100

    SceneChange
    Added a scenechange boxcollider behind the door.
    
    FPS controller need a trigger-collider for it to work. To change which scene is loaded, look in script sceneChangeCollider.cs

---
+++ ScalienAbduction/Assets/Scripts/sceneChangeCollider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneChangeCollider : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("Player Entered");
+        if (collision.gameObject.tag == "Player")
+            SceneManager.LoadScene(2);
+    }
+}
