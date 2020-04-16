64e4e0aedff2a94bc4885a7b3aa4cddc528b0c9f
Mads Aagaard <math15@student.aau.dk>
2020-04-16 19:19:32 +0200

    Door Done + a little extra

--- ScalienAbduction/Assets/Scripts/doorLogicUnlock.cs
+++ ScalienAbduction/Assets/Scripts/doorLogicUnlock.cs
@@ -10,61 +10,143 @@ public class doorLogicUnlock : MonoBehaviour
     public List<GameObject> floorDiodes = new List<GameObject>();
 
     public float speed = 5f;
+    private int doorDis = 10; //Door Displacement
+    private Vector3 doorDisSpotRight;
+    private Vector3 doorDisSpotLeft;
+
+    private bool moving = false;
+    private bool rightDone = false;
+    private bool leftDone = false;
+    private bool open = false;
 
     void Start()
     {
         floorDiodes.AddRange(GameObject.FindGameObjectsWithTag("floorDiode"));
-        transform.localRotation = Quaternion.Euler(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(doorKeySnap.mediumUnlocked == true && doorKeySnap.smallAUnlocked == true && doorKeySnap.smallBUnlocked == true)
+        if (!moving)
         {
-            StartCoroutine(doorLightUp());
+            if (doorKeySnap.mediumUnlocked == true && doorKeySnap.smallAUnlocked == true && doorKeySnap.smallBUnlocked == true)
+            {
+                if(!open)
+                {
+                    open = !open;
+                    StartCoroutine(TurnOnOffDiodes());
+                }
+            }
+            if (doorKeySnap.mediumUnlocked == false || doorKeySnap.smallAUnlocked == false || doorKeySnap.smallBUnlocked == false)
+            {
+                if (open)
+                {
+                    open = !open;
+                    StartCoroutine(TurnOnOffDiodes());
+                }
+            }
         }
-        if (doorKeySnap.mediumUnlocked == false || doorKeySnap.smallAUnlocked == false || doorKeySnap.smallBUnlocked == false)
+        if (rightDone && leftDone)
         {
-            StartCoroutine(doorLockUp());
+            StopCoroutine(RightDoorOpenLock());
+            StopCoroutine(LeftDoorOpenLock());
+            rightDone = false;
+            leftDone = false;
+            moving = false;
         }
     }
 
-    IEnumerator doorLightUp()
+    IEnumerator TurnOnOffDiodes()
     {
+        moving = true;
         for (int i = 0; i < floorDiodes.Count; i++)
         {
-            var cubeRenderer = floorDiodes[i].GetComponent<Renderer>();
-            //cubeRenderer.material.SetColor("_Tint", Color.green);
-
+            if (open)
+            {
+                floorDiodes[i].GetComponent<Renderer>().material.SetColor("_Tint", Color.green);
+            }
+            else
+            {
+                floorDiodes[i].GetComponent<Renderer>().material.SetColor("_Tint", Color.white);
+            }
+            
             yield return new WaitForSeconds(0.1f);
-
             if (i == floorDiodes.Count-1)
             {
-                var doorRendererRight = exitDoorRight.GetComponentInChildren<Renderer>();
-                doorRendererRight.material.SetColor("_Tint", Color.green);
-
-                exitDoorLeft.transform.localPosition = Vector3.Lerp(transform.position, transform.position + new Vector3(-10, 0, 0) , Time.deltaTime*speed);
+                StartCoroutine(RightDoorOpenLock());
+                StartCoroutine(LeftDoorOpenLock());
+                break;
             }
         }
+        while (!rightDone || !leftDone)
+        {
+            yield return null;
+        }
+        yield return new WaitForSeconds(3f);
+
     }
 
-    IEnumerator doorLockUp()
+    IEnumerator RightDoorOpenLock()
     {
-        for (int i = 0; i < floorDiodes.Count; i++)
+        if(open)
         {
-            var cubeRenderer = floorDiodes[i].GetComponent<Renderer>();
-            cubeRenderer.material.SetColor("_Tint", Color.white);
-
-            yield return new WaitForSeconds(0.1f);
-
-            if (i == floorDiodes.Count - 1)
+            doorDisSpotRight = exitDoorRight.transform.localPosition - new Vector3(doorDis, 0, 0);
+            while (exitDoorRight.transform.localPosition != doorDisSpotRight)
             {
-                var doorRenderer = exitDoorRight.GetComponent<Renderer>();
-                //doorRenderer.material.SetColor("_BaseColor", Color.red);
-
-                transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(0, 0, 0), Time.deltaTime * speed);
+                Debug.Log("DoorDisSpot " + doorDisSpotRight + "  andcurrent position");
+                exitDoorRight.transform.localPosition = Vector3.Lerp(exitDoorRight.transform.localPosition, doorDisSpotRight, Time.deltaTime * speed);
+                yield return null;
             }
         }
+        else
+        {
+            doorDisSpotRight = exitDoorRight.transform.localPosition + new Vector3(doorDis, 0, 0);
+            while (exitDoorRight.transform.localPosition != doorDisSpotRight)
+            {
+                exitDoorRight.transform.localPosition = Vector3.Lerp(exitDoorRight.transform.localPosition, doorDisSpotRight, Time.deltaTime * speed);
+                yield return null;
+            }
+        }
+        while (!rightDone || !leftDone)
+        {
+            if (!rightDone)
+            {
+                rightDone = true;
+            }
+            yield return null;
+        }
+       
+        yield return new WaitForSeconds(3f);
+    }
+
+    IEnumerator LeftDoorOpenLock()
+    {
+        if(open)
+        {
+            doorDisSpotLeft = exitDoorLeft.transform.localPosition + new Vector3(doorDis, 0, 0);
+            while (exitDoorLeft.transform.localPosition != doorDisSpotLeft)
+            {
+                exitDoorLeft.transform.localPosition = Vector3.Lerp(exitDoorLeft.transform.localPosition, doorDisSpotLeft, Time.deltaTime * speed);
+                yield return null;
+            }
+        }
+        else
+        {
+            doorDisSpotLeft = exitDoorLeft.transform.localPosition - new Vector3(doorDis, 0, 0);
+            while (exitDoorLeft.transform.localPosition != doorDisSpotLeft)
+            {
+                exitDoorLeft.transform.localPosition = Vector3.Lerp(exitDoorLeft.transform.localPosition, doorDisSpotLeft, Time.deltaTime * speed);
+                yield return null;
+            }
+        }
+        while (!rightDone || !leftDone)
+        {
+            if (!leftDone)
+            {
+                leftDone = true;
+            }
+            yield return null;
+        }
+        yield return new WaitForSeconds(3f);
     }
 }
