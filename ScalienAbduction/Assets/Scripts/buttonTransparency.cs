8ee2b75bc5f7f9b7afabf6bae44de94edc8be8f0
Unknown <ndanie16@student.aau.dk>
2020-04-18 05:03:23 +0200

    MenuScreen Done!
    Time for Level 4

---
+++ ScalienAbduction/Assets/Scripts/buttonTransparency.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonTransparency : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Color col = GetComponent<Image>().color;
+        col.a = 0;
+        GetComponent<Image>().color = col;
+    }
+}
